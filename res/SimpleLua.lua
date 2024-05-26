f4e9ee122a9416724ba21fda387265c08a3b4d0a
gk646 <lukas.gilch@gmx.de>
2024-05-27 00:50:51 +0200

    init

---
+++ res/SimpleLua.lua
@@ -0,0 +1,11 @@
+
+
+function update_entities(frame)
+    if frame % 10 == 0 then
+        set_position(1, 100 * frame, 200 * frame) -- Move entity 1
+    end
+    local a = 5
+    local b = 150
+
+    local c = a+b
+end
