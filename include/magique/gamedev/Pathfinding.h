19ccc413d64d9d0750a4113948d1fcac86aca5ca
gk646 <gk646@proton.me>
2024-09-04 22:00:58 +0200

    untested changes! started implementing pathfinding backend refactored hashgrid insert func into rasterize function

--- include/magique/gamedev/Pathfinding.h
+++ include/magique/gamedev/Pathfinding.h
@@ -1,20 +1,26 @@
 #ifndef MAGIQUE_PATHFINDING_H
 #define MAGIQUE_PATHFINDING_H
 
+#include <vector>
 #include <magique/core/Types.h>
 
 //-----------------------------------------------
 // Pathfinding Module
 //-----------------------------------------------
 // .....................................................................
-// 
+//
 // .....................................................................
 
 namespace magique
 {
+    // Finds the next tile you should move to to reach the end point the fastest
+    // Equal to the next point when searching the shortest path
+    Point GetNextTile(Point start, Point end);
 
-  Point FindPath(Point start, Point end);
+    // Finds and assigns the tiles along the shortest path to the given vector - excluding the start tile
+    // Failure: if no path can be found return an empty vector
+    void GetShortestPath(std::vector<Point>& path, Point start, Point end);
 
-}
+} // namespace magique
 
 #endif //MAGIQUE_PATHFINDING_H
