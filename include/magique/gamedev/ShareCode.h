2e77919a15816e6d0293f5be57e157a9d3ee4b53
gk646 <gk646@proton.me>
2024-09-14 23:44:53 +0200

    finished sharecode implementation - added: import and export with tests - missing: prettify code, can be nulltermineted eearly and unreadable -> only use readable ascii

--- include/magique/gamedev/ShareCode.h
+++ include/magique/gamedev/ShareCode.h
@@ -1,38 +1,89 @@
 #ifndef MAGIQUE_SHARECODE_H
 #define MAGIQUE_SHARECODE_H
 
+#include <string>
 #include <vector>
 #include <magique/internal/InternalTypes.h>
 #include <magique/internal/Macros.h>
 #include <magique/util/Logging.h>
 
 //-------------------------------
-// ShareCode
+// ShareCode Module
 //-------------------------------
 // ................................................................................
-//
-// Note: The format has to outlive all the data object that it produces
+// This module allows to generate a space efficient string representation of configurable data
+// It works by creating a format that specifies the different chunks (e.g. size and type) of the sharecode
+// Then, by passing data values in the same format allows to generate the space efficient representation
+// The data is packed on a bit level avoiding ANY unnecessary padding
+// When importing
+// Intended Workflow:
+//          - 1. Create a format: Add the properties you want and specify the maximum range
+//          - 2. Set the data   : Use getFormatData() to get the correct data object and set the property values
+//          - 3. Get ShareCode  : Now you can generate the sharecode by passing the data to the format
+//          - 4. Import         : Import the sharecode with the format that produced it to get the contained data
+// Note: The format has to outlive all the data objects that it produces
 // ................................................................................
 
 namespace magique
 {
-
-    // Returns the version of the ShareCode generator
+    // Returns the version of the sharecode generator
     uint8_t GetShareCodeGeneratorVersion();
 
     struct ShareCode final
     {
-        ~ShareCode();
+        // Creates a share code by copying the given string
+        // Failure: internal code will be null
+        static ShareCode FromString(const char* str);
 
-        // Returns the version of the generator that produced this ShareCode
-        uint8_t getVersion();
+        //----------------- ACCESS -----------------//
+
+        // Returns the version of the generator this sharecode was made with
+        [[nodiscard]] uint8_t getVersion() const;
 
         // Returns the checksum of this ShareCode
-        uint16_t getCheckSum();
+        [[nodiscard]] uint16_t getCheckSum() const;
+
+        // Returns the sharecode copied into a string
+        [[nodiscard]] std::string getAsString() const;
+
+        // Returns the sharecode directly without making a copy
+        [[nodiscard]] const char* getCode() const;
+
+        // Returns true if the given sharecode is valid
+        [[nodiscard]] bool getIsValid() const;
+
+        ~ShareCode();
 
     private:
+        explicit ShareCode(const char* code);
         const char* code = nullptr;
+        ShareCode() = default;
         friend struct ShareCodeFormat;
+    };
+
+    struct ShareCodeProperty final
+    {
+        // Returns true if the property has the given type
+        [[nodiscard]] bool isInteger() const;
+        [[nodiscard]] bool isFloat() const;
+        [[nodiscard]] bool isString() const;
+
+        // Returns the value
+        // IMPORTANT: program will crash when you call the wrong type getter
+        //            -> check if its integer first before calling getInteger()
+        [[nodiscard]] int getInteger() const;
+        [[nodiscard]] float getFloat() const;
+        [[nodiscard]] const char* getString() const;
+
+    private:
+        internal::ShareCodePropertyType type{};
+        union
+        {
+            char* string = nullptr;
+            float floating;
+            int32_t integer;
+        };
+        friend struct ShareCodeData;
     };
 
     struct ShareCodeData final
@@ -44,6 +95,19 @@ namespace magique
         // Sets the data of the property at the given index
         template <typename T>
         void setData(int index, T data);
+
+        // Returns the data of the property with the given name
+        // Note: The given type has to be larger and fit the underlying type (e.g. int for Integer)
+        // Failure: returns null or the minimal value of the type
+        [[nodiscard]] ShareCodeProperty getData(const char* name) const;
+
+        // Returns the data of the property at the given index
+        // Note: The given type has to be larger and fit the underlying type (e.g. int for Integer)
+        // Failure: returns null or the minimal value of the type
+        [[nodiscard]] ShareCodeProperty getData(int index) const;
+
+        // Returns the total number of properties
+        [[nodiscard]] int getSize() const;
 
     private:
         template <typename T>
@@ -59,15 +123,14 @@ namespace magique
 
         //----------------- PROPERTIES -----------------//
 
-        // Adds a new property with that stores an integer number
+        // Adds a new property with that stores an integer number - bit size is the minimal size that can hold the given amount
         //      - amount: maximum amount of different numbers the property can hold (e.g. 255, 4000, 65536)
-        void addIntegerProperty(uint64_t amount, const char* name = nullptr);
+        void addIntegerProperty(uint32_t amount, const char* name = nullptr);
 
-        // Adds a new property with that stores floating point number
-        //      - amount: maximum amount of different numbers the property can hold (e.g. 255, 4000, 65536)
-        void addFloatProperty(double amount, const char* name = nullptr);
+        // Adds a new property with that stores floating point number - always 32 bits
+        void addFloatProperty(const char* name = nullptr);
 
-        // Adds a new property that can store text up to the given maximum length
+        // Adds a new property that can store text up to the given maximum length - variable length
         void addTextProperty(const char* name = nullptr);
 
         // Returns true if the property with the given name was successfully removed
@@ -79,21 +142,22 @@ namespace magique
         //----------------- EXPORT -----------------//
 
         // Returns an empty data instance of this format
-        // Note: You need to set the value of each property in the data in order to export it
+        // Note: You need (should) to set the value of each property in the data in order to export it
         [[nodiscard]] ShareCodeData getFormatData() const;
 
-        // Returns the string share code that holds the given data
-        // Note: The generated string can ONLY be imported by the same format!l
-        // Failure: on error returns an empty string
-        [[nodiscard]] ShareCode getShareCodeString(const ShareCodeData& data) const;
+        // Returns the sharecode that holds the given data
+        // Note: The sharecode can ONLY be imported by the format that generated it!
+        // Failure: returns invalid sharecode
+        [[nodiscard]] ShareCode getShareCode(const ShareCodeData& data) const;
 
         //----------------- IMPORT -----------------//
 
-        // Returns the checksum of the format - useful to determine which format the ShareCode comes from
-        // The checksum of a ShareCode matches the checksum of the format it was generated by!
+        // Returns the checksum of the format - useful to determine which format the sharecode comes from
+        // The checksum of a sharecode matches the checksum of the format it was generated by!
         [[nodiscard]] uint16_t getCheckSum() const;
 
-        // Returns the data for this format filled with the parsed values from the ShareCode
+        // Returns the data for this format filled with the parsed values from the given string sharecode
+        // Failure: returns empty data
         [[nodiscard]] ShareCodeData importFromString(const ShareCode& shareCode) const;
 
     private:
@@ -108,6 +172,8 @@ namespace magique
 {
     namespace internal
     {
+        int GetShareCodeCharacterSize();
+
         template <typename T>
         int getBitSize(T num)
         {
@@ -133,7 +199,8 @@ namespace magique
             }
             else if constexpr (std::is_same_v<T, const char*>)
             {
-                return strlen(num) * 8;
+                // +1 for the terminator
+                return (strlen(num) + 1) * GetShareCodeCharacterSize();
             }
             else
             {
@@ -171,10 +238,10 @@ namespace magique
     void ShareCodeData::setData(internal::ShareCodePropertyData& p, T data)
     {
         const int bits = internal::getBitSize(data);
-        const char* fmt = "Size of the data is greater than specified: %s | Maximum/Actual: %d/%d";
+        const char* fmt = "Trying to assign data that is bigger than specified: %s | Maximum/Actual: %d/%d";
         if constexpr (std::is_integral_v<T>)
         {
-            M_SHARECODE_CHECKTYPE(internal::ShareCodeBlockType::INTEGER, "Property is not of integer type: %s")
+            M_SHARECODE_CHECKTYPE(internal::ShareCodePropertyType::INTEGER, "Property is not of integer type: %s")
             if (bits > p.bits)
             {
                 LOG_WARNING(fmt, p.name, p.bits, bits);
@@ -184,7 +251,7 @@ namespace magique
         }
         else if constexpr (std::is_floating_point_v<T>)
         {
-            M_SHARECODE_CHECKTYPE(internal::ShareCodeBlockType::FLOATING, "Property is not of floating type: %s")
+            M_SHARECODE_CHECKTYPE(internal::ShareCodePropertyType::FLOATING, "Property is not of floating type: %s")
             if (bits > p.bits)
             {
                 LOG_WARNING(fmt, p.name, p.bits, bits);
@@ -194,7 +261,7 @@ namespace magique
         }
         else if constexpr (std::is_same_v<const char*, T>)
         {
-            M_SHARECODE_CHECKTYPE(internal::ShareCodeBlockType::STRING, "Property is not of string type: %s")
+            M_SHARECODE_CHECKTYPE(internal::ShareCodePropertyType::STRING, "Property is not of string type: %s")
             free(p.string);
             p.bits = bits;
             p.string = data == nullptr ? nullptr : strdup(data);
---
+++ src/internal/utils/BitUtil.h
@@ -0,0 +1,31 @@
+#ifndef MAGIQUE_BITUTIL_H
+#define MAGIQUE_BITUTIL_H
+
+namespace magique
+{
+    // Writes 'bitsToWrite' many bits from val to 'byte' starting at the given offset
+    template <typename T>
+    int WriteBits(T val, const int bitsToWrite, char& byte, const int offset)
+    {
+        const int bitsAvailableInByte = 8 - offset;
+        const int bitsToWriteNow = std::min(bitsToWrite, bitsAvailableInByte);
+        T mask = (1 << bitsToWriteNow) - 1;
+        byte &= ~(mask << offset);
+        byte |= ((val & mask) << offset);
+        return bitsToWriteNow;
+    }
+
+    // Read 'bitsToRead' many bits into val from 'byte' starting at the given offset
+    template <typename T>
+    int ReadBits(T& val, const int bitsToRead, const char byte, const int offset)
+    {
+        const int bitsAvailableInByte = 8 - offset;
+        const int bitsToReadNow = std::min(bitsToRead, bitsAvailableInByte);
+        T mask = (1 << bitsToReadNow) - 1;
+        T extractedBits = (byte >> offset) & mask;
+        val |= extractedBits;
+        return bitsToReadNow;
+    }
+} // namespace magique
+
+#endif //MAGIQUE_BITUTIL_H
